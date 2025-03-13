6331c8aa2a74f2097a61fb98161680e005d26999
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-03-13 16:45:51 -0500

    add functionality to submit book form

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
     this.id = crypto.randomUUID();
 }
 
@@ -11,48 +12,51 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-function displayBooks(library) {
-    const display = document.querySelector(".display");
-    library.forEach(book => {
-        let bookDiv = document.createElement("div");
-        bookDiv.id = book.id;
-        let title = document.createElement("h3");
-        let author = document.createElement("p");
-        let pages = document.createElement("p");
-        let buttonContainer = document.createElement("div");
-        buttonContainer.classList.add("buttonContainer");
-        let isReadContainer = document.createElement("div");
-        isReadContainer.textContent = "Read? ";
-        let isReadButton = document.createElement("button");
-        isReadButton.classList.add("isReadButton");
-        let removeBook = document.createElement("button");
-        removeBook.textContent = "Remove";
-        removeBook.addEventListener("click", ()=>{
-            let parentBook = removeBook.closest(".book");
-            let idx = 0;
-            for (let i = 0; i < myLibrary.length; i++) {
-                if (myLibrary[i].id===parentBook.id) {
-                    idx = i;
-                }
-            }
-            myLibrary.splice(idx, 1);
-            parentBook.remove();
-        });
+function displayBook(book) {
 
-        bookDiv.className = "book";
-        title.textContent = book.title;
-        author.textContent = book.author;
-        pages.textContent = book.pages;
-        bookDiv.appendChild(title);
-        bookDiv.appendChild(author);
-        bookDiv.appendChild(pages);
-        isReadContainer.appendChild(isReadButton);
-        buttonContainer.appendChild(isReadContainer);
-        buttonContainer.appendChild(removeBook);
-        bookDiv.appendChild(buttonContainer);
-        bookDiv.style.backgroundColor=getRandomColor();
-        display.appendChild(bookDiv);
-    })
+}
+
+function displayBook(book) {
+    const display = document.querySelector(".display");
+
+    let bookDiv = document.createElement("div");
+    bookDiv.id = book.id;
+    let title = document.createElement("h3");
+    let author = document.createElement("p");
+    let pages = document.createElement("p");
+    let buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("buttonContainer");
+    let isReadContainer = document.createElement("div");
+    isReadContainer.textContent = "Read? ";
+    let isReadButton = document.createElement("button");
+    isReadButton.classList.add("isReadButton");
+    let removeBook = document.createElement("button");
+    removeBook.textContent = "Remove";
+    removeBook.addEventListener("click", ()=>{
+        let parentBook = removeBook.closest(".book");
+        let idx = 0;
+        for (let i = 0; i < myLibrary.length; i++) {
+            if (myLibrary[i].id===parentBook.id) {
+                idx = i;
+            }
+        }
+        myLibrary.splice(idx, 1);
+        parentBook.remove();
+    });
+
+    bookDiv.className = "book";
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    bookDiv.appendChild(title);
+    bookDiv.appendChild(author);
+    bookDiv.appendChild(pages);
+    isReadContainer.appendChild(isReadButton);
+    buttonContainer.appendChild(isReadContainer);
+    buttonContainer.appendChild(removeBook);
+    bookDiv.appendChild(buttonContainer);
+    bookDiv.style.backgroundColor=getRandomColor();
+    display.appendChild(bookDiv);
 }
 
 function getRandomColor() {
@@ -68,10 +72,23 @@ addBookButton.addEventListener("click", ()=>{
     form.style.visibility = form.style.visibility==="hidden" ? "visible" : "hidden";
 });
 
+const form = document.querySelector("form");
+form.addEventListener("submit", e=>{
+    e.preventDefault();
+    const formData = new FormData(form);
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = formData.get("numPages");
+    let book = new Book(title, author, pages);
+    addBookToLibrary(book);
+    displayBook(book);
+    form.reset();
+});
+
 let book1 = new Book("LOTR", "JRRT", 1064);
 let book2 = new Book("The Hobbit", "JRRT", 342);
 let book3 = new Book("Alias Grace", "Atwood", 500);
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
-displayBooks(myLibrary);
+myLibrary.forEach(book=>displayBook(book));
