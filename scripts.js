25bf1809ad80b24389c94a8c2850947acae78dd4
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-03-13 15:36:19 -0500

    add a lot of functionality

--- scripts.js
+++ scripts.js
@@ -18,6 +18,15 @@ function displayBooks(library) {
         let title = document.createElement("h3");
         let author = document.createElement("p");
         let pages = document.createElement("p");
+        let buttonContainer = document.createElement("div");
+        buttonContainer.classList.add("buttonContainer");
+        let isReadContainer = document.createElement("div");
+        isReadContainer.textContent = "Read? ";
+        let isReadButton = document.createElement("button");
+        isReadButton.classList.add("isReadButton");
+        let removeBook = document.createElement("button");
+        removeBook.textContent = "Remove";
+
         bookDiv.className = "book";
         title.textContent = book.title;
         author.textContent = book.author;
@@ -25,17 +34,27 @@ function displayBooks(library) {
         bookDiv.appendChild(title);
         bookDiv.appendChild(author);
         bookDiv.appendChild(pages);
+        isReadContainer.appendChild(isReadButton);
+        buttonContainer.appendChild(isReadContainer);
+        buttonContainer.appendChild(removeBook);
+        bookDiv.appendChild(buttonContainer);
         bookDiv.style.backgroundColor=getRandomColor();
         display.appendChild(bookDiv);
     })
 }
 
 function getRandomColor() {
-    const r = Math.floor(Math.random()*256);
-    const g = Math.floor(Math.random()*256);
-    const b = Math.floor(Math.random()*256);
+    const r = Math.floor(Math.random()*200);
+    const g = Math.floor(Math.random()*200);
+    const b = Math.floor(Math.random()*200);
     return `rgb(${r},${g},${b})`
 }
+
+const addBookButton = document.querySelector("#addBook");
+addBookButton.addEventListener("click", ()=>{
+    let form = document.querySelector("form");
+    form.style.visibility = form.style.visibility==="hidden" ? "visible" : "hidden";
+});
 
 let book1 = new Book("LOTR", "JRRT", 1064);
 let book2 = new Book("The Hobbit", "JRRT", 342);
