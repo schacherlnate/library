f1a309e574ff2f472ee973d24b0b7e31110b45d0
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-03-12 21:44:35 -0500

    make code to display books

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 const myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
+    this.pages = pages;
     this.id = crypto.randomUUID();
 }
 
@@ -10,5 +11,36 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-let book1 = new Book("LOTR", "JRRT");
-console.log(book1);
+function displayBooks(library) {
+    const display = document.querySelector(".display");
+    library.forEach(book => {
+        let bookDiv = document.createElement("div");
+        let title = document.createElement("h3");
+        let author = document.createElement("p");
+        let pages = document.createElement("p");
+        bookDiv.className = "book";
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+        bookDiv.appendChild(title);
+        bookDiv.appendChild(author);
+        bookDiv.appendChild(pages);
+        bookDiv.style.backgroundColor=getRandomColor();
+        display.appendChild(bookDiv);
+    })
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`
+}
+
+let book1 = new Book("LOTR", "JRRT", 1064);
+let book2 = new Book("The Hobbit", "JRRT", 342);
+let book3 = new Book("Alias Grace", "Atwood", 500);
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+displayBooks(myLibrary);
